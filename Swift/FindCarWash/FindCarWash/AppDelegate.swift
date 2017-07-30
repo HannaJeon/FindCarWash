7e58e95fe1130058da84b41c5f8c4c955f186f3f
HannaJeon <jks7psj@gmail.com>
2017-07-31 01:39:58 +0900

    세차장 위치 마커 생성

--- Swift/FindCarWash/FindCarWash/AppDelegate.swift
+++ Swift/FindCarWash/FindCarWash/AppDelegate.swift
@@ -20,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         let mapKey = valueForAPIKey(keyname: "GoogleMapAPI")
         GMSServices.provideAPIKey(mapKey)
         
+        locationManager.requestWhenInUseAuthorization()
+        
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -58,18 +60,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         return key
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let networking = Networking()
-        let location = manager.location?.coordinate
-        if let currentLocation = location {
-            Location.sharedInstance = Location(latitude: currentLocation.latitude, longitude: currentLocation.longitude)
-            print(Location.sharedInstance.latitude)
-            print(Location.sharedInstance.longitude)
-//            NotificationCenter.default.post(name: NSNotification.Name("currentLocation"), object: self, userInfo: ["currentLocation" : userLocation])
-            networking.getCarWash(latitude: currentLocation.latitude, longitude: currentLocation.longitude)
-        }
-        locationManager.stopUpdatingLocation()
-    }
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        let networking = Networking()
+//        let location = manager.location?.coordinate
+//        if let currentLocation = location {
+//            Location.sharedInstance = Location(latitude: currentLocation.latitude, longitude: currentLocation.longitude)
+//            print(Location.sharedInstance.latitude)
+//            print(Location.sharedInstance.longitude)
+////            NotificationCenter.default.post(name: NSNotification.Name("currentLocation"), object: self, userInfo: ["currentLocation" : userLocation])
+//            networking.getCarWash(latitude: currentLocation.latitude, longitude: currentLocation.longitude)
+//        }
+//        locationManager.stopUpdatingLocation()
+//    }
 
 }
 
--- Swift/FindCarWash/FindCarWash/MapViewController.swift
+++ Swift/FindCarWash/FindCarWash/MapViewController.swift
@@ -14,18 +14,19 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     let locationManager = CLLocationManager()
     let networking = Networking()
     var carWashInfo = [CarWash]()
-    var currentLocation = Location()
+    var currentLocation = Location.sharedInstance
+    var mapView: GMSMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        if CLLocationManager.locationServicesEnabled() {
-//            locationManager.delegate = self
-//            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//            locationManager.startUpdatingLocation()
-//        }
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
         
-        NotificationCenter.default.addObserver(self, selector: #selector(getCurrentLocation(_:)), name: NSNotification.Name("currentLocation"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(getCarWashInfo(_:)), name: NSNotification.Name("finishedGetCarWash"), object: networking)
     }
 
@@ -34,9 +35,9 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    override func loadView() {
-        let camera = GMSCameraPosition.camera(withLatitude: currentLocation.latitude, longitude: currentLocation.longitude, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+    func drawCurrentLocation() {
+        let camera = GMSCameraPosition.camera(withLatitude: currentLocation.latitude, longitude: currentLocation.longitude, zoom: 15.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
         view = mapView
         
@@ -48,31 +49,39 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         mapView.settings.myLocationButton = true
     }
     
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        
-//        let location = manager.location?.coordinate
-//        
-//        if let currentLocation = location {
-//            self.currentLocation = Location(latitude: currentLocation.latitude, longitude: currentLocation.longitude)
-//            networking.getCarWash(latitude: currentLocation.latitude, longitude: currentLocation.longitude)
-//        }
-//        locationManager.stopUpdatingLocation()
-//    }
-    
-    func getCurrentLocation(_ notification: Notification) {
-        if let userInfo = notification.userInfo as? [String:Location] {
-            if let location = userInfo["currentLocation"] {
-                currentLocation = location
-            }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = manager.location?.coordinate
+        
+        if let currentLocation = location {
+            self.currentLocation = Location(latitude: currentLocation.latitude, longitude: currentLocation.longitude)
+            networking.getCarWash(latitude: currentLocation.latitude, longitude: currentLocation.longitude)
         }
+        drawCurrentLocation()
+        locationManager.stopUpdatingLocation()
     }
     
     func getCarWashInfo(_ notification: Notification) {
         if let userInfo = notification.userInfo as? [String:[CarWash]] {
             if let carWash = userInfo["CarWash"] {
                 carWashInfo = carWash
+                insertCarWashMarker()
             }
         }
+    }
+    
+    func insertCarWashMarker() {
+        carWashInfo.forEach { (carWash) in
+            createMaker(latitude: carWash.위도, longitude: carWash.경도, title: carWash.사업장명, snippet: carWash.소재지도로명주소)
+        }
+    }
+    
+    func createMaker(latitude: Double, longitude: Double, title: String, snippet: String) {
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        marker.title = title
+        marker.snippet = snippet
+        marker.map = mapView
     }
     
 }
